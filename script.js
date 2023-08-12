0cf22fc77f24815b6b4e72ae1c3e5d13a89327ac
Mohammad Shezaan Ansari <shezaanansari2210@gmail.com>
2023-08-12 17:09:40 +0530

    second commit

--- script.js
+++ script.js
@@ -51,20 +51,22 @@ gsap.to(`#main>svg`,{
 
 var tl = gsap.timeline()
 
-tl.from(`#page1>img`,{
+tl.from(`#main>svg`,{
+    opacity:0,
+    y:-50,
+    duration:.8,
+    delay:0.4
+
+})
+
+.from(`#page1>img`,{
     scale:.5,
     delay:.5,
     duration:1.5,
     ease:Power4.easeOut,
 })
 
-.from(`#main>svg`,{
-    opacity:0,
-    y:-50,
-    duration:.8,
-    delay:-0.4
 
-})
 .from(`#nav`,{
     opacity:0,
     y:-50,
@@ -211,7 +213,7 @@ gsap.from(`#page4>#container>.elem`,{
         end:`60% 30%`,
         scroller:`#main`,
         scrub:2,
-        markers:true,
+        // markers:true,
        
     },
     // stagger:.2,
@@ -220,3 +222,274 @@ gsap.from(`#page4>#container>.elem`,{
 
 })  
 
+
+var parent = document.querySelector(`#swiper-main`)
+var left = document.querySelector(`#swipers>#left>i`)
+var right = document.querySelector(`#swipers>#right>i`)
+var two = document.querySelector(`.inner-swiper2`)
+var three = document.querySelector(`.inner-swiper3`)
+
+var swiping = 0;
+
+right.addEventListener("click",function(){
+    swiping += 1500
+    parent.scrollTo(swiping,0)
+    two.style.height = `80vh`
+    two.style.width = `80vw`
+    // three.style.height = `80vh`
+    // two.style.scale = `2`
+})
+
+left.addEventListener("click",function(){
+    swiping -= 1500
+    parent.scrollTo(swiping,0)
+    two.style.height = `40vh`
+    // three.style.height = `40vh`
+})
+
+gsap.to(`#page6>#top1, #page6>#top2`,{
+    scrollTrigger:{
+        trigger:`#page6`,
+        scroller:`#main`,
+        start:`-10% bottom`,
+        end:`150% 20%`,
+        // markers:true,
+        scrub:true,
+        
+    },
+    transform: `translateX(-20%) scaleY(-1) `,
+    duration:5
+
+})
+var clutter = " ";
+document.querySelector("#page6>h1").textContent.split("").forEach(function(dets){
+    clutter += `<span>${dets}</span>`
+document.querySelector("#page6>h1").innerHTML = clutter;
+
+
+});
+
+gsap.to("#page6>h1>span",{
+    scrollTrigger:{
+        trigger:`#page6>h1`,
+        start:`20% bottom`,
+        end:`60% top`,
+        scroller:`#main`,
+        scrub:true,
+        // markers:true,
+        
+    },
+    stagger:.3,
+    color:`#E3E3C4`,
+    duration:.3,
+    ease:Power4.easeOut,
+})
+
+tl.from(`#page6>p`,{
+    scrollTrigger:{
+        trigger:`#page6`,
+        start:`10% 80%`,
+        end:`60% 30%`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+    },
+    opacity:0,
+    y:50
+
+
+}) 
+tl.from(`#page6>#flex>.inner-flex1,#page6>#flex>.inner-flex2,#page6>#flex>.inner-flex3`,{
+    scrollTrigger:{
+        trigger:`#page6`,
+        start:`40% 80%`,
+        end:`60% 30%`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+    },
+    opacity:0,
+    y:50,
+    stagger:.3
+
+
+}) 
+
+gsap.to(`#page6>#btm1, #page6>#btm2`,{
+    scrollTrigger:{
+        trigger:`#page6>#btm1`,
+        scroller:`#main`,
+        start:`-10% bottom`,
+        end:`150% 20%`,
+        // markers:true,
+        scrub:true,
+        
+    },
+    transform: `translateX(-20%)  `,
+    duration:5
+
+})
+
+var clutter = " ";
+document.querySelector("#page7>h1").textContent.split("").forEach(function(dets){
+    clutter += `<span>${dets}</span>`
+document.querySelector("#page7>h1").innerHTML = clutter;
+
+
+});
+
+gsap.to("#page7>h1>span",{
+    scrollTrigger:{
+        trigger:`#page7>h1`,
+        start:`50% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:.3,
+        // markers:true,
+        
+    },
+    stagger:.3,
+    color:`#434B33`,
+    duration:.3,
+    ease:Power4.easeOut,
+})
+
+gsap.to("#page7>#left",{
+    scrollTrigger:{
+        trigger:`#page7>#centr`,
+        start:`-30% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    transform : `translateX(-50%)`
+})
+gsap.to("#page7>#right",{
+    scrollTrigger:{
+        trigger:`#page7>#centr`,
+        start:`-30% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    transform : `translateX(50%)`
+})
+
+gsap.to("#page7>#centr",{
+    scrollTrigger:{
+        trigger:`#page7>#centr`,
+        start:`-30% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:true,
+        // markers:true,
+        
+    },
+    opacity:1
+})
+
+gsap.to(`#page9>#top1, #page9>#top2`,{
+    scrollTrigger:{
+        trigger:`#page9>#top1`,
+        scroller:`#main`,
+        start:`-10% bottom`,
+        end:`150% 20%`,
+        // markers:true,
+        scrub:true,
+        
+    },
+    transform: `translateX(-20%) scaleY(-1) `,
+    duration:5
+
+})
+gsap.from("#page9>p",{
+    scrollTrigger:{
+        trigger:`#page9>p`,
+        start:`-30% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:true,
+        // markers:true,
+        
+    },
+    opacity:0,
+    y:50
+})
+
+gsap.from("#page9>#grid9>.two, #grid9>.three, #grid9>.one",{
+    scrollTrigger:{
+        trigger:`#page9>#grid9`,
+        start:`-30% bottom`,
+        end:`40% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    opacity:0,
+    y:50,
+    stagger:.3
+})
+
+var tl2 = gsap.timeline();
+
+tl2.from(`#page10>.flex1>h1,#page10>.flex1>p`,{
+    scrollTrigger:{
+        trigger:`#page10>.flex1`,
+        start:`-10% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    opacity:0,
+    y:50,
+    stagger:.1
+})
+
+.from(`#page10>#centr>.left10,#page10>#centr>.right10`,{
+    scrollTrigger:{
+        trigger:`#page10>#centr`,
+        start:`-10% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    opacity:0,
+    y:50,
+    stagger:.3
+})
+.from(`#page10>#centr2>.left10,#page10>#centr2>.right10`,{
+    scrollTrigger:{
+        trigger:`#page10>#centr2`,
+        start:`-10% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:2,
+        // markers:true,
+        
+    },
+    opacity:0,
+    y:50,
+    stagger:.3
+})
+
+gsap.from(`#page11>.centr`,{
+    scrollTrigger:{
+        trigger:`#page11>.centr`,
+        start:`-10% bottom`,
+        end:`80% top`,
+        scroller:`#main`,
+        scrub:true,
+        // markers:true,
+    },
+    opacity:0,
+    y:30
+})
